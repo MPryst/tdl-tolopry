c5760725e875cc67acc9e3af164adb45448e9e18
Maximiliano Prystupiuk <mprystupiuk@fi.uba.ar>
2023-06-05 13:05:10 -0300

    Added a build script for truffle

---
+++ build.sh
@@ -0,0 +1,8 @@
+truffle compile
+truffle migrate
+truffle console 
+
+# Execute the following after:
+    # let instance = await ToLoPryCoin.deployed()
+    # let accounts = await web3.eth.getAccounts()
+    # instance.name()
